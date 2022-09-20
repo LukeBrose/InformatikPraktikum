21ce9be259ae0d61889765399f81fa90f16a8767
Laura <laura.wuensch@uni-bayreuth.de>
2022-09-20 22:55:25 +0200

    Gegner stirbt bei der Collision auf Kopf
    Gegner hat Collider auf der oberen Seite, wenn Spieler*in draufspringt stribt der Gegner

--- Informatik Praktikum/Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
+++ Informatik Praktikum/Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
@@ -16,8 +16,8 @@ namespace Pathfinding {
 	public class AIDestinationSetter : VersionedMonoBehaviour {
 		/// <summary>The object that the AI should move to</summary>
 		public Transform target;
-		IAstarAI ai;
-		public bool insight = false; 
+		protected IAstarAI ai;
+		public bool insight = false;
 
 		void OnEnable () {
 			ai = GetComponent<IAstarAI>();
@@ -33,11 +33,10 @@ namespace Pathfinding {
 		}
 
 		/// <summary>Updates the AI's destination every frame</summary>
-		void Update () {
+		protected virtual void Update () {
 			// Die Update Methode wird nur genutzt, wenn der Gegner in Sicht ist (siehe AI_Patrol)
-			if (target != null && ai != null)
+			if (target != null && ai != null )
 				ai.destination = target.position;
-
 
 		}
 	}
